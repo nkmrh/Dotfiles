90b207be8f41121bdb8044d170ddd1f9ae18a173
nkmrh <hajimeet@me.com>
2015-12-14 18:11:33 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DOTPATH=~/.dotfiles
 
-git clone git@github.com:nkmrh/dotfiles.git ~/.dotfiles
+git clone git@github.com:nkmrh/dotfiles.git $DOTPATH
 
 for f in .??*
 do
@@ -13,6 +13,6 @@ done
 
 ln -sf "$DOTPATH"/bin "$HOME"/bin
 
-mkdir -p .vim/bundle/
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 vim +NeoBundleInstall +qall
