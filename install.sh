3042366235dde64912f40b91bd256152263b1f5e
Hajime Nakamura <hajime-nakamura@cookpad.com>
2015-12-14 14:54:31 +0900

    Update.

--- install.sh
+++ install.sh
@@ -8,3 +8,10 @@ do
     [[ "$f" == ".DS_Store" ]] && continue
     ln -sf "$DOTPATH"/"$f" "$HOME"/"$f"
 done
+
+cd .vim
+mkdir bundle/
+cd bundle/
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+vim +NeoBundleInstall +qall
+cd ~
