33c4a2f802f67ac2b096314765b6ec33eb153d8d
nkmrh <hajimeet@me.com>
2015-12-14 20:56:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if type "brew" > /dev/null 2>&1
+if [type "brew" > /dev/null 2>&1]
 then
 else
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
